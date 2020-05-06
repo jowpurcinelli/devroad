c36fb0c3c8ff5f11b7444ea2a8748b1297c04c94
João Pedro Purcinelli <53058735+jowpurcinelli@users.noreply.github.com>
2020-05-06 09:25:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+//This function would reduce the size of the box. Could be used later?
+// function hit(elm) {
+//     if (elm.scale_val == null ) {
+//         elm.scale_val = 0.8;
+//     }
+//     elm.scale_val = Math.round(elm.scale_val * 10 ) / 10;
+//     if (elm.scale_val <= 0 ) {
+//         elm.remove();
+//     } else {
+//         elm.style.transform = "scale(" + elm.scale_val + " ) " ;
+        
+//     }
+
+//         elm.scale_val -= 0.2;
+// }
